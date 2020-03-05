a77412a84455b141589a07e87badc7b4ccb05437
Mark <markmdvd@gmail.com>
2020-03-05 15:34:15 -0800

    sphere

--- main2.js
+++ main2.js
@@ -106,12 +106,7 @@ function main2() {
         return;
     }
 
-    // Set the vertex coordinates, the color and the normal
-    var n = initVertexBuffers(gl);
-    if (n < 0) {
-        console.log('Failed to set the vertex information');
-        return;
-    }
+
 
     // Set the clear color and enable the depth test
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -156,7 +151,17 @@ function main2() {
 
 
     var tick = function() {
+        // Clear color and depth buffer
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
         currentAngle = animate(currentAngle);  // Update the rotation angle
+
+        // Set the vertex coordinates, the color and the normal
+        var n = initVertexBuffers(gl);
+        if (n < 0) {
+            console.log('Failed to set the vertex information');
+            return;
+        }
 
         mvMatrix.setLookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
         gl.uniformMatrix4fv(u_MvMatrix,false, mvMatrix.elements);
@@ -174,11 +179,32 @@ function main2() {
         normalMatrix.transpose();
         gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
 
-        // Clear color and depth buffer
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
         // Draw the cube
         gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+
+        var j = initSphereVertexBuffers(gl);
+        if (j < 0) {
+            console.log('Failed to set the vertex information');
+            return;
+        }
+        mvMatrix.setLookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+        gl.uniformMatrix4fv(u_MvMatrix,false, mvMatrix.elements);
+        // Calculate the model matrix
+        modelMatrix.setRotate(currentAngle, 0, 1, 0); // Rotate around the y-axis
+        // Pass the model matrix to u_ModelMatrix
+        gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+
+        // Pass the model view projection matrix to u_MvpMatrix
+        mvpMatrix.set(vpMatrix).multiply(modelMatrix);
+        gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+        // Pass the matrix to transform the normal based on the model matrix to u_NormalMatrix
+        normalMatrix.setInverseOf(modelMatrix);
+        normalMatrix.transpose();
+        gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
+
+        // Draw the cube
+        gl.drawElements(gl.TRIANGLES, j, gl.UNSIGNED_SHORT, 0);
 
         requestAnimationFrame(tick, canvas); // Request that the browser ?calls tick
     };
@@ -195,8 +221,6 @@ function initVertexBuffers(gl) {
     //  |/      |/
     //  v2------v3
     // Coordinates
-
-
 
 
     var vertices = new Float32Array([
@@ -246,6 +270,92 @@ function initVertexBuffers(gl) {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
     return indices.length;
+}
+
+function initSphereVertexBuffers(gl) { // Create a sphere
+    var SPHERE_DIV = 52;
+
+    var i, ai, si, ci;
+    var j, aj, sj, cj;
+    var p1, p2;
+
+    var positions = [];
+    var indices = [];
+
+    // Generate coordinates
+    for (j = 0; j <= SPHERE_DIV; j++) {
+        aj = j * Math.PI / SPHERE_DIV;
+        sj = Math.sin(aj);
+        cj = Math.cos(aj);
+        for (i = 0; i <= SPHERE_DIV; i++) {
+            ai = i * 2 * Math.PI / SPHERE_DIV;
+            si = Math.sin(ai);
+            ci = Math.cos(ai);
+
+            positions.push(si * sj);  // X
+            positions.push(cj);       // Y
+            positions.push(ci * sj);  // Z
+        }
+    }
+
+    // Generate indices
+    for (j = 0; j < SPHERE_DIV; j++) {
+        for (i = 0; i < SPHERE_DIV; i++) {
+            p1 = j * (SPHERE_DIV+1) + i;
+            p2 = p1 + (SPHERE_DIV+1);
+
+            indices.push(p1);
+            indices.push(p2);
+            indices.push(p1 + 1);
+
+            indices.push(p1 + 1);
+            indices.push(p2);
+            indices.push(p2 + 1);
+        }
+    }
+
+    // Write the vertex property to buffers (coordinates and normals)
+    // Same data can be used for vertex and normal
+    // In order to make it intelligible, another buffer is prepared separately
+    if (!initSphereArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3)) return -1;
+    if (!initSphereArrayBuffer(gl, 'a_Normal', new Float32Array(positions), gl.FLOAT, 3))  return -1;
+
+    // Unbind the buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    // Write the indices to the buffer object
+    var indexBuffer = gl.createBuffer();
+    if (!indexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    return indices.length;
+}
+
+function initSphereArrayBuffer(gl, attribute, data, type, num) {
+    // Create a buffer object
+    var buffer = gl.createBuffer();
+    if (!buffer) {
+        console.log('Failed to create the buffer object');
+        return false;
+    }
+    // Write date into the buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    // Assign the buffer object to the attribute variable
+    var a_attribute = gl.getAttribLocation(gl.program, attribute);
+    if (a_attribute < 0) {
+        console.log('Failed to get the storage location of ' + attribute);
+        return false;
+    }
+    gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+    // Enable the assignment of the buffer object to the attribute variable
+    gl.enableVertexAttribArray(a_attribute);
+
+    return true;
 }
 
 function initArrayBuffer(gl, attribute, data, num, type) {
